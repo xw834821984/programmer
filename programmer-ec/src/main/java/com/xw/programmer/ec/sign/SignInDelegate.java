a78fcfef84888245ca0f5f1bce248c0299cbc3c3
纳兹 <834821984@qq.com>
2017-12-25 17:55:55 +0800

    登陆注册串连

---
+++ programmer-ec/src/main/java/com/xw/programmer/ec/sign/SignInDelegate.java
@@ -0,0 +1,80 @@
+package com.xw.programmer.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.xw.programmer.ec.R;
+import com.xw.programmer.ec.R2;
+import com.xw.programmer_nucleus.delegetes.LatteDelegate;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by nazi on
+ * date： 2017/12/25
+ */
+
+public class SignInDelegate extends LatteDelegate {
+
+    @BindView(R2.id.edit_sign_in_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_in_password)
+    TextInputEditText mPassword = null;
+
+    @OnClick(R2.id.btn_sign_in)
+    void onClickSignIn(){
+        if (checkForm()) {
+            Toast.makeText(getContext(),"登陆成功",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @OnClick(R2.id.icon_sign_in_wechat)
+    void onClickWeChat(){
+
+            Toast.makeText(getContext(),"被点击" ,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @OnClick(R2.id.tv_link_sign_up)
+    void onClickLink(){
+        start(new SignUpDelegate());
+    }
+
+    private boolean checkForm() {
+        final String email = mEmail.getText().toString();
+        final String password = mPassword.getText().toString();
+
+        boolean isPass = true;
+
+        //检查邮箱格式
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return isPass;
+    }
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_in;
+    }
+
+    @Override
+    public void onBindview(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
--- programmer-ec/src/main/java/com/xw/programmer/ec/sign/SignUpDelegate.java
+++ programmer-ec/src/main/java/com/xw/programmer/ec/sign/SignUpDelegate.java
@@ -52,6 +52,12 @@ public class SignUpDelegate extends LatteDelegate {
         }
     }
 
+
+    @OnClick(R2.id.tv_link_sign_in)
+    void onClickLink(){
+        start(new SignInDelegate());
+    }
+
     private boolean checkForm() {
         final String name = mName.getText().toString();
         final String email = mEmail.getText().toString();
